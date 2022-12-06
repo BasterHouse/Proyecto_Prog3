3a6cc15a53683ae8a32ca9edea69e5aad8675c5c
Usuario <Usuario@DESKTOP-JL6618N>
2022-12-06 12:27:10 +0100

    6/12

--- Prueba_Proyecto/src/ventanas/VentanaDeustomusic.java
+++ Prueba_Proyecto/src/ventanas/VentanaDeustomusic.java
@@ -12,6 +12,8 @@ import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JList;
 import clave.DeustoMusic;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
 
 public class VentanaDeustomusic extends JFrame{
 	public VentanaDeustomusic(DeustoMusic deustomusic) {
@@ -22,21 +24,21 @@ public class VentanaDeustomusic extends JFrame{
 		getContentPane().setLayout(null);
 		
 		JScrollPane multimedias = new JScrollPane();
-		multimedias.setBounds(167, 18, 229, 56);
+		multimedias.setBounds(167, 18, 269, 86);
 		getContentPane().add(multimedias);
 		
 		JLabel titulomultimedias = new JLabel("Todo el contenido🎵");
 		multimedias.setColumnHeaderView(titulomultimedias);
 		
 		JScrollPane favoritas = new JScrollPane();
-		favoritas.setBounds(167, 99, 229, 56);
+		favoritas.setBounds(167, 134, 269, 86);
 		getContentPane().add(favoritas);
 		
 		JLabel titulofavoritas = new JLabel("Favoritas❤");
 		favoritas.setColumnHeaderView(titulofavoritas);
 		
 		JScrollPane playlists = new JScrollPane();
-		playlists.setBounds(167, 185, 229, 52);
+		playlists.setBounds(167, 251, 269, 86);
 		getContentPane().add(playlists);
 		
 		JLabel tituloplaylists = new JLabel("Tus Playlists📃");
@@ -44,25 +46,25 @@ public class VentanaDeustomusic extends JFrame{
 		
 		JLabel titulogeneral = new JLabel("DeustoMusic");
 		titulogeneral.setFont(new Font("Stencil", Font.PLAIN, 16));
-		titulogeneral.setBounds(10, 11, 132, 30);
+		titulogeneral.setBounds(10, 11, 147, 38);
 		getContentPane().add(titulogeneral);
 		
 		JButton buscar = new JButton("Buscar");
-		buscar.setBounds(10, 66, 97, 23);
+		buscar.setBounds(10, 77, 117, 38);
 		getContentPane().add(buscar);
 		
 		JButton perfil = new JButton("Perfil");
-		perfil.setBounds(10, 112, 97, 23);
+		perfil.setBounds(10, 137, 117, 35);
 		getContentPane().add(perfil);
 		
 		JButton cerrarsesion = new JButton("Cerrar Sesión");
-		cerrarsesion.setBounds(10, 162, 97, 23);
+		cerrarsesion.setBounds(10, 203, 117, 35);
 		getContentPane().add(cerrarsesion);
 		
 		JButton informacion = new JButton("Información");
-		informacion.setBounds(10, 208, 97, 23);
+		informacion.setBounds(10, 271, 117, 35);
 		getContentPane().add(informacion);
-		
+
 		JList list = new JList();
 		list.setBounds(167, 270, 229, 41);
 		DefaultListModel<Multimedia> modelo = new DefaultListModel<>();
@@ -71,5 +73,7 @@ public class VentanaDeustomusic extends JFrame{
 		}
 		list.setModel(modelo);
 		getContentPane().add(list);
+		
+		
 	}
 }
