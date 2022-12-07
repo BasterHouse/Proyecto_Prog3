7cfbd2d1b7d05e2080f08ba7389e45236f643387
Usuario <Usuario@DESKTOP-JL6618N>
2022-12-07 18:48:35 +0100

    7/12

--- Prueba_Proyecto/src/ventanas/VentanaPrinc.java
+++ Prueba_Proyecto/src/ventanas/VentanaPrinc.java
@@ -6,8 +6,20 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 import javax.swing.SwingConstants;
+
+import clases.Cancion;
+import clases.Multimedia;
+import clases.Podcast;
+import clases.Usuario;
+import clave.DeustoMusic;
+import clave.GestorBBDD;
+import test.TestUsuario;
+
 import javax.swing.JTextField;
 
 public class VentanaPrinc extends JFrame{
@@ -22,9 +34,32 @@ public class VentanaPrinc extends JFrame{
 	private String gmail;
 	private String contraseña;
 	
+	DeustoMusic deustomusic = new DeustoMusic();
+	GestorBBDD gestor = new GestorBBDD();
+	List<Cancion> canciones = new ArrayList<Cancion>();
+	List<Podcast> podcasts = new ArrayList<Podcast>();
+	List<Usuario> usuarios = new ArrayList<Usuario>();
+	
+	
+	TreeMap<String, ArrayList<Multimedia>> listademedia = deustomusic.inicializar();
+	
+	
 	
 
 	public VentanaPrinc() {
+		
+		for (Multimedia cancion : listademedia.get("Canciones")) {
+			if (cancion instanceof Cancion) {
+				canciones.add((Cancion)cancion);
+			}
+		}
+		
+		for (Multimedia podcast : listademedia.get("Podcasts")) {
+			if (podcast instanceof Podcast) {
+				podcasts.add((Podcast)podcast);
+			}
+		}
+		
 		setTitle("DeustoMusic");
 		setSize(600, 400);
 		setLocationRelativeTo(null);
@@ -97,18 +132,55 @@ public class VentanaPrinc extends JFrame{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
+			nick = textNick.getText();
+			gmail = textGmail.getText();
+			contraseña = textContraseña.getText();
+				if (nick == null || gmail == null || contraseña == null) {
+					System.err.println("Rellene todos los huecos"); 
+				}else {
+					Usuario nuevo = new Usuario(usuarios.size(), nick, gmail, contraseña);
+					gestor.insertarDatosUsuario(nuevo);
+				}
 				
 			}
+				
+			
 		});
+		deustomusic.llenarPlaylist();
+		deustomusic.usuarioPlaylist();
 		
+		//BASE DE DATOS DE LAS CANCIONES
+		gestor.crearBBDDCancion();
+		gestor.insertarDatosCancion(canciones.toArray(new Cancion[canciones.size()]));
+		canciones = gestor.obtenerDatosCancion();
+		gestor.actualizarNombreCancion(canciones.get(0), "BorjaTeQueremos");
+		canciones = gestor.obtenerDatosCancion();
+		gestor.borrarDatosCancion();
+		gestor.borrarBBDDCancion();
+		
+		//BASE DE DATOS DE LOS PODCASTS
+		gestor.crearBBDDPodcast();
+		gestor.insertarDatosPodcast(podcasts.toArray(new Podcast[podcasts.size()]));
+		podcasts = gestor.obtenerDatosPodcast();
+		gestor.actualizarNombrePodcast(podcasts.get(0), "BorjaTeQueremos");
+		podcasts = gestor.obtenerDatosPodcast();
+		gestor.borrarDatosPodcast();
+		gestor.borrarBBDDPodcast();
+		
+		VentanaDeustomusic ventanamusic = new VentanaDeustomusic(deustomusic);
+		ventanamusic.setVisible(true);	
 	}
+	
+		
+
+
+	
 	
 	public void regis() {
 		nick = textNick.getText();
 		gmail = textGmail.getText();
 		contraseña = textContraseña.getText();
+	
 		
 
 	}
@@ -122,5 +194,10 @@ public class VentanaPrinc extends JFrame{
 		dos.setVisible(true);
 		tres.setVisible(true);
 
+	}
+	
+	public static void main(String[] args) {
+		VentanaPrinc v = new VentanaPrinc();
+		v.setVisible(true);
 	}
 }
